1faadc7cf2a8560436e351e99ee24ac9db5f167d
Felix <felix@xingren.com>
2021-01-26 15:23:32 +0800

    feat: 更新图片

--- common/echarts.js
+++ common/echarts.js
@@ -8,56 +8,56 @@ const echarts = [
     id: guid(),
     title: '折线图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/line-simple.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/line-simple.webp',
     option: require('./data/basic-line-chart'),
   },
   {
     id: guid(),
     title: '柱状图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/bar-background.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/bar-background.webp',
     option: require('./data/bar-with-background'),
   },
   {
     id: guid(),
     title: '饼图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/pie-custom.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/pie-simple.webp',
     option: require('./data/customized-pie'),
   },
   {
     id: guid(),
     title: '散点图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/bubble-gradient.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/scatter-simple.webp',
     option: require('./data/bubble-chart'),
   },
   {
     id: guid(),
     title: 'K 线图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/candlestick-sh.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/candlestick-simple.webp',
     option: require('./data/shanghai-index'),
   },
   {
     id: guid(),
     title: '雷达图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/radar-aqi.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/radar.webp',
     option: require('./data/aqi-radar-chart'),
   },
   {
     id: guid(),
     title: '盒须图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/boxplot-light-velocity.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/data-transform-aggregate.webp',
     option: require('./data/boxplot-light-velocity'),
   },
   {
     id: guid(),
     title: '热力图',
     thumbnail:
-      'https://echarts.cdn.apache.org/examples//data/thumb/heatmap-cartesian.jpg',
+      'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples/data/thumb/heatmap-cartesian.webp',
     option: require('./data/heatmap-on-cartesian'),
   },
   {
