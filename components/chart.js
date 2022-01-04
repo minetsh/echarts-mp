9beaee588c65a33542f61348577b164d787af422
Felix <felix@xingren.com>
2022-01-04 12:09:37 +0800

    feat: components

---
+++ components/chart.js
@@ -0,0 +1,35 @@
+// components/chart.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    option: Object,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    // option: {
+    //   xAxis: {
+    //     type: 'category',
+    //     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    //   },
+    //   yAxis: {
+    //     type: 'value',
+    //   },
+    //   series: [
+    //     {
+    //       data: [150, 230, 224, 218, 135, 147, 260],
+    //       type: 'line',
+    //     },
+    //   ],
+    // },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
--- pages/home/index.js
+++ pages/home/index.js
@@ -3,6 +3,21 @@ const { echarts } = require('../../common/echarts');
 Page({
   data: {
     echarts,
+    option: {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line',
+        },
+      ],
+    },
   },
   onTap(event) {
     wx.navigateTo({
